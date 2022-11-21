26f23eaf56d7328b4c712e7665a66e6885aeb40e
axelrod <fasolshik@yahoo.com>
2022-11-21 22:56:40 +0600

    UI design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,24 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-
   end
 
   def new
+    render :layout => nil
   end
 
   def edit
   end
 
   def show
-    @user = User.all
+    @user = User.find(params[:id])
+    @questions = @user.questions.order(created_at: :desc)
+    # @new_question = @user.questions.build
+    @new_question = Question.new
+  end
 
+  private
+  def load_user
+    @user ||= User.find params[:id]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   root 'users#index'
 
+  # Ресурс пользователей (экшен destroy не поддерживается)
   resources :users, except: [:destroy]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # get 'show' => 'users#show'
+  # Ресурс сессий (только три экшена :new, :create, :destroy)
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # Ресурс вопросов (кроме экшенов :show, :new, :index)
+  resources :questions, except: [:show, :new, :index]
+
+
+  # Синонимы путей — в дополнение к созданным в ресурсах выше.
+  #
+  # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
+  # их удалим.
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
+
