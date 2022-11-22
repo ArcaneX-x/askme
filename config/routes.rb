ec86e2e74cb6fe45fa1f856ea34fa215cde6e369
axelrod <fasolshik@yahoo.com>
2022-11-22 14:33:01 +0600

    Register function

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,16 @@
 Rails.application.routes.draw do
   root 'users#index'
 
-  # Ресурс пользователей (экшен destroy не поддерживается)
   resources :users, except: [:destroy]
-
-  # Ресурс сессий (только три экшена :new, :create, :destroy)
-  resources :sessions, only: [:new, :create, :destroy]
-
-  # Ресурс вопросов (кроме экшенов :show, :new, :index)
+  # resources :sessions, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
-
 
   # Синонимы путей — в дополнение к созданным в ресурсах выше.
   #
   # Для любознательных: синонимы мы добавили, чтобы показать одну вещь и потом
   # их удалим.
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
+  # get 'sign_up' => 'users#new'
+  # get 'log_out' => 'sessions#destroy'
+  # get 'log_in' => 'sessions#new'
 end
 
