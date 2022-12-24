b649bc83275eccc759b3ee834080abcbab294764
axelrod <fasolshik@yahoo.com>
2022-12-24 20:46:44 +0600

    reset model

--- app/models/hashtag.rb
+++
@@ -1,7 +0,0 @@
-class Hashtag < ApplicationRecord
-  REGEXP = /#[[:word:]-]+/
-  has_many :questions, dependent: :destroy
-  validates :text, presence: true, length: { maximum: 30}, uniqueness: true
-
-
-end
