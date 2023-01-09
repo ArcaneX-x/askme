83a3aa223cdf6170c2402c27a3ba0e707a545f1a
axelrod <fasolshik@yahoo.com>
2023-01-09 23:06:10 +0600

    deleted tmp file

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'users#index'
+  root "users#index"
 
   resources :users
-  resources :sessions, only: %i[new create destroy]
+  resource :session, only: %i[new create destroy]
   resources :questions, except: %i[show new index]
   resources :hashtags, only: [:show], param: :text
 
-  get '/users/:id/questions' => 'users#questions', as: :user_questions
-  get 'specialists' => 'users#specialists'
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
-  get 'destroy' => 'users#destroy'
+  get "/users/:id/questions" => "users#questions", as: :user_questions
+  get "specialists" => "users#specialists"
 end
