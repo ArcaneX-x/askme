1a3df3c216e1bb8307c459237d062cb87ad518bd
axelrod <fasolshik@yahoo.com>
2023-01-27 18:06:31 +0600

    Refactoring

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'active_storage/engine'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_mailbox/engine'
-require 'action_text/engine'
-require 'action_view/railtie'
-require 'action_cable/engine'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -39,10 +39,10 @@ module Askme
     config.generators do |g|
       g.test_framework nil # to skip test framework
     end
-    config.time_zone = 'Moscow'
+    config.time_zone = "Moscow"
 
     config.i18n.default_locale = :en
-    config.i18n.locale = :ru
+    config.i18n.locale = :en
 
     config.i18n.fallbacks = [:en]
   end
